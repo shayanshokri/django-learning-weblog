2523d324f68c750d3018ad866bcdd519e8f4e578
shayanshokri <shokrishayan1@gmail.com>
2024-08-16 01:30:41 +0330

    show posts table in admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'title', 'created_time', 'publish_date', 'updated_time', 'active'
+
+    ]
+
+
+admin.site.register(Post, PostAdmin)
