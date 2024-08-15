e02dcf44a163060e2230b2d4ef9f922992b37a79
shayanshokri <shokrishayan1@gmail.com>
2024-08-16 01:21:34 +0330

    create post models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField(blank=True)
+    active = models.BooleanField(default=False)
+    publish_date = models.DateField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
