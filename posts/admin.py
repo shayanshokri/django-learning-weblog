2d0db925ff93b5e4feb392cd2e032598005d66ac
shayanshokri <shokrishayan1@gmail.com>
2024-08-16 02:19:24 +0330

    create inline class for comments to show in form of posts in admin file

--- posts/admin.py
+++ posts/admin.py
@@ -3,12 +3,20 @@ from django.contrib import admin
 from .models import Post, Comment
 
 
+class CommentAdminInline(admin.TabularInline):
+    model = Comment
+    fields = ['text']
+    extra = 0
+
+
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         'id', 'title', 'created_time', 'publish_date', 'updated_time', 'active'
     ]
+    inlines = [CommentAdminInline]
 
 
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = [
         'id', 'post', 'created_time', 'updated_time'
@@ -16,4 +24,4 @@ class CommentAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Comment, CommentAdmin)
+# admin.site.register(Comment, CommentAdmin)
