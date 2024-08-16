0270bccf91fe646de68544408b54176f3c8083f8
shayanshokri <shokrishayan1@gmail.com>
2024-08-16 22:00:13 +0330

    add new view to get all post from DB

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def home(request):
     return HttpResponse("Welcome to my Weblog.")
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'posts/post_list.html', context=context)
