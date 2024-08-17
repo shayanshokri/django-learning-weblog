536d60accd230f39509fedce9fafd224dcc2a17f
shayanshokri <shokrishayan1@gmail.com>
2024-08-17 22:39:07 +0330

    define new url for post creation

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('posts', views.post_list),
-    path('post/<int:pk>', views.post_details)
+    path('post/<int:pk>', views.post_details),
+    path('post/create/', views.post_create),
 ]
