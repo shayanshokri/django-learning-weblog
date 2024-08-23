ed6d212ba2491e732055ccb90b56e004f688952b
shayanshokri <shokrishayan1@gmail.com>
2024-08-23 23:18:56 +0330

    define new manager

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class PostLiveManager(models.Manager):
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(id = 1)
+        return queryset
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=50)
@@ -9,6 +16,9 @@ class Post(models.Model):
     publish_date = models.DateField(null=True, blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
+
+    objects = models.Manager()
+    live = PostLiveManager()
 
     def __str__(self):
         return self.title
