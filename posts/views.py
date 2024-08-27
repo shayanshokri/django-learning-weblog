cac055cd790a26c568f3d55077dc65a91052adcb
shayanshokri <shokrishayan1@gmail.com>
2024-08-27 10:44:16 +0330

    convert views to APIView Class

--- posts/views.py
+++ posts/views.py
@@ -1,69 +1,51 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
-from .models import Post, Comment
-from .forms import PostForm
-from django.views import generic
+from django.http import Http404
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.response import Response
 
+from .models import Post
 from .serializers import PostSerializers
 
 
-@api_view(['GET', 'POST'])
-def home(request):
-    try:
-        pk = request.query_params.get('pk')
-        posts = Post.objects.get(pk=pk)
-        serializer = PostSerializers(posts)
-        print(serializer)
-        print(serializer.data)
-        return Response(serializer.data)
-        # return Response({'posts': posts})
-    except Exception as E:
-        return Response({'details': 'post not found!'}, status=status.HTTP_400_BAD_REQUEST)
+class PostListView(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serialized_posts = PostSerializers(posts, many=True)
+        return Response(serialized_posts.data)
+
+    def post(self, request):
+        serialized_data = PostSerializers(data=request.data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data, status=status.HTTP_201_CREATED)
+        return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-def post_list(request):
-    posts = Post.objects.all()
-    context = {'posts': posts}
-    return render(request, 'posts/post_list.html', context=context)
+class PostDetailView(APIView):
 
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+        except Exception as E:
+            raise Http404
+        return post
 
-def post_details(request, pk):
-    # try:
-    #     post = Post.objects.get(id=pk)
-    # except Post.DoesNotExist:
-    #     return HttpResponseNotFound(' Post not found!')
-    post = get_object_or_404(Post, pk=pk)
-    comments = Comment.objects.filter(post=post)
-    context = {'post': post, 'comments': comments}
-    return render(request, 'posts/post_details.html', context=context)
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serialized_post = PostSerializers(post)
+        return Response(serialized_post.data)
 
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serialized_post = PostSerializers(post, data=post)
+        if serialized_post.is_valid():
+            serialized_post.save()
+            return Response(serialized_post.data, status=status.HTTP_200_OK)
+        return Response(serialized_post.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class PostDetails(generic.DetailView):
-    model = Post
-    template_name = 'posts/post_details.html'
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def get_context_data(self, **kwargs):
-        context = super(PostDetails, self).get_context_data()
-        context['comments'] = Comment.objects.filter(post=kwargs['object'].pk)
-        return context
-
-
-def post_create(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            Post.objects.create(**form.cleaned_data)
-            return HttpResponseRedirect('/posts')
-    else:
-        form = PostForm()
-        return render(request, 'posts/post_create.html', {'form': form})
-
-
-class PostList(generic.ListView):
-    queryset = Post.objects.all()
-    template_name = 'posts/post_list.html'
-    context_object_name = 'posts'
