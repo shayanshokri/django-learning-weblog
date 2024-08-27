d1b6fac5ba46dc01d0cd84cdae256d0b22b4e862
shayanshokri <shokrishayan1@gmail.com>
2024-08-27 11:51:42 +0330

    change url format based on viewsets

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view()),
-    path('<int:pk>/', views.PostDetailView.as_view())
+    # path('', views.PostListView.as_view()),
+    # path('<int:pk>/', views.PostDetailView.as_view())
+    path('', views.PostView.as_view({
+        'get': 'list',
+        'post': 'create'
+    })),
+    path('<int:pk>/', views.PostView.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'
+    }))
 ]
