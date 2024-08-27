d6dc5cc55aa2e1392bbae5d20f327c3bc4fa969d
shayanshokri <shokrishayan1@gmail.com>
2024-08-27 11:56:56 +0330

    use routers to define urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,16 +1,23 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
 from . import views
+
+router = DefaultRouter()
+router.register(r'', views.PostView, basename='posts')
 
 urlpatterns = [
     # path('', views.PostListView.as_view()),
     # path('<int:pk>/', views.PostDetailView.as_view())
-    path('', views.PostView.as_view({
-        'get': 'list',
-        'post': 'create'
-    })),
-    path('<int:pk>/', views.PostView.as_view({
-        'get': 'retrieve',
-        'put': 'update',
-        'delete': 'destroy'
-    }))
+    # path('', views.PostView.as_view({
+    #     'get': 'list',
+    #     'post': 'create'
+    # })),
+    # path('<int:pk>/', views.PostView.as_view({
+    #     'get': 'retrieve',
+    #     'put': 'update',
+    #     'delete': 'destroy'
+    # }))
+    path('', include(router.urls))
 ]
