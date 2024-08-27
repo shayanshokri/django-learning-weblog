72432d4b1030a8927daa2506c83700b177761147
shayanshokri <shokrishayan1@gmail.com>
2024-08-27 13:02:15 +0330

    add permissions

--- posts/views.py
+++ posts/views.py
@@ -6,13 +6,16 @@ from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import mixins
 from rest_framework import viewsets
+from rest_framework import permissions
 
 from .models import Post
 from .serializers import PostSerializers
 
+
 class PostView(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializers
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 # class PostListView(generics.ListCreateAPIView):
 #     queryset = Post.objects.all()
